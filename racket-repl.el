11ea7f6d0ae51ca479ae2eb47e32c1bd52d56267
Greg Hendershott <greghendershott@gmail.com>
2014-09-03 15:54:31 -0400

    Show doc (if any) not just bluebox.
    Also ,describe now returns plain text rather than an association list,
    since none of the other information is really useful. Even the
    Definition and Documentation buttons can be done using racket-doc and
    racket-visit-definition commands on the symbol.
    
    One drawback is that my conversion from HTML to plain text is not always
    pleasing. So next I'm going to try having ,describe return HTML text,
    then use libxml-parse-html-region to parse it and shr-insert-document to
    display it. I may need to simplify the Scribble HTML, however.

--- racket-repl.el
+++ racket-repl.el
@@ -244,7 +244,10 @@ Keep original window selected."
   "Major mode for Racket REPL.
 \\{racket-repl-mode-map}"
   (racket--variables-for-both-modes)
-  (setq-local comint-prompt-regexp "^[^<>#\"\n]*> +")
+  ;; comint-prompt-regexp is important to get exactly right for
+  ;; comint-redirect-send-command-to-process as used by
+  ;; racket--eval/buffer and friends.
+  (setq-local comint-prompt-regexp "^[^<>#\"'\n ]*> +$")
   ;; (setq-local comint-use-prompt-regexp t)
   ;; (setq-local comint-prompt-read-only t)
   (setq-local mode-line-process nil)
