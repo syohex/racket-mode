ba4a407004ca7ec49d174df083a4641659a4d31d
Greg Hendershott <greghendershott@gmail.com>
2014-09-19 19:20:37 -0400

    Commit d6f3141 relaxed the prompt regexp too much.
    Need to require at least one space following the >.
    
    This change broke racket--get-namespace-symbols: Its output would stop
    going to the redirected output buffer and start spilling into the REPL
    buffer instead.

--- racket-repl.el
+++ racket-repl.el
@@ -251,7 +251,7 @@ Keep original window selected."
   ;; comint-prompt-regexp is important to get exactly right for
   ;; comint-redirect-send-command-to-process as used by
   ;; racket--eval/buffer and friends.
-  (setq-local comint-prompt-regexp "^[^<>#\"' \n]*> *")
+  (setq-local comint-prompt-regexp "^[^<>#\"' \n]*> +")
   ;; (setq-local comint-use-prompt-regexp t)
   ;; (setq-local comint-prompt-read-only t)
   (setq-local mode-line-process nil)
